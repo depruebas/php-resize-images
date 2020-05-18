4df3773cf059a12495328b3ce08da119e43c4e9d
depruebas <alex.arenols@depruebas.com>
2020-05-18 17:12:48 +0200

    update resize

--- resize_images.php
+++ resize_images.php
@@ -1,25 +1,37 @@
 <?php
 
+	# usage:  php resize_images.php 0.5
 
 	$filename = '';
+
+	# Directorio donde estan la imagenes a redimensionar
 	$directorio_in = dirname(__FILE__) . "/in/";
+
+	# Directorio de salida
 	$directorio_out = dirname(__FILE__) . "/out/";
+
+	# Porcentaje que queremos reducir, se pasa como parametro
 	$porcentaje = $argv[1];
 
-
+	# Obtenemos todos los ficheros del directorio de entrada
 	$files = scandir( $directorio_in);
 
+	# Si el directori de salida no existe lo creamos
 	if ( !file_exists( $directorio_out))
 	{
 	    mkdir( $directorio_out, 0764, true);
 	}
 
+	# Recorremos el array de ficheros del directorio in
 	foreach ($files as $f)
 	{
 
 		$filename = $directorio_in."/".$f;
+
+		# Miramos si el fichero es una imagen
 		$mine_type = mime_content_type( $filename);
 
+		# Y si es una imagen le pasamos el proceso de redimension
 		if ( $mine_type == "image/jpeg")
 		{
 
@@ -40,6 +52,7 @@
 			$file = pathinfo( $filename);
 			$new_filename = $file['filename']."_p.".$file['extension'];
 
+			# Creamos la nueva imagen
 			imagejpeg($thumb, $directorio_out.$new_filename);
 
 			echo $directorio_out.$new_filename."\n";
